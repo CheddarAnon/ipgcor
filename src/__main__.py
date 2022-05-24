3e5583cecae17b24b4d0106d349b47f8225c54f2
Jason Dusek <jason.dusek@gmail.com>
2022-05-23 21:08:04 -0500

    Adjust log formatting

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,9 @@
 from datetime import datetime
+import sys
 
 import click
 from loguru import logger
 
-from .correlate import correlate_things
 
 @click.command(
     context_settings=dict(
@@ -27,11 +27,24 @@ from .correlate import correlate_things
 @click.version_option()
 def main(working_directory, authentication_file):
     start = datetime.utcnow()
+    setup_logging()
     logger.info("Program start.")
     logger.info(f"{dict(working_directory=working_directory)}")
     logger.info(f"{dict(authentication_file=authentication_file)}")
-    correlate_things()
+    # correlate_things()
     end = datetime.utcnow()
     logger.info(f"Program end after: {end - start}")
+
+def setup_logging():
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        level="INFO",
+        format=("<green>{time:YYYY-MM-DDTHH:mm:ss.SSS!UTC}Z</green> "
+                "| <level>{level: <8}</level> "
+                "| <cyan>{name}</cyan>:<cyan>{function}</cyan>:"
+                "<cyan>{line}</cyan> "
+                "- <level>{message}</level>")
+    )
 
 main()
